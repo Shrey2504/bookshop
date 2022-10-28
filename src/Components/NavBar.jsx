d34cab78c1fa4ec0fd0decab6a7f19adf5a7a479
Sanskriti Gupta <guptasj@rknec.edu>
2022-10-28 11:48:01 +0530

    Navbar edited

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -21,18 +21,20 @@ export default function Header() {
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
-      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-20">
+      
+      <header className="flex justify-between items-center  max-w-6xl mx-auto">
         <div>
           <img
-            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            src="https://rails-assets-us.bookshop.org/assets/logo-a52621fe944d907a0a91448f35b41eca07947302711d35c3322a99144928f1aa.svg"
             alt="logo"
-            className="h-5 cursor-pointer"
+            className="h-5 cursor-pointer "
+            // height={450} 
+            // width={350}
             onClick={() => navigate("/")}
           />
         </div>
-        <div>
-          <ul className="flex space-x-10">
+        <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute("/") && "text-black border-b-red-500"
@@ -43,12 +45,30 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/offers") && "text-black border-b-red-500"
+                pathMatchRoute("/contactus") && "text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/offers")}
+              onClick={() => navigate("/contactus")}
             >
-              Offers
+              Contact Us
             </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/ebooks") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/ebooks")}
+            >
+              E-books
+            </li>
+           
+           
+          </ul>
+        <div > 
+          
+        </div>
+        <div>
+
+          <ul className="flex space-x-10">
+            
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 (pathMatchRoute("/signin") || pathMatchRoute("/profile")) &&
@@ -57,6 +77,15 @@ export default function Header() {
               onClick={() => navigate("/profile")}
             >
               {pageState}
+            </li>
+
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/cart") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/cart")}
+            >
+             Cart
             </li>
           </ul>
         </div>
